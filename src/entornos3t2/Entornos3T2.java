e15e56c2d659d7b416c9f564c24b82dc12041bd0
Nestor de Frutos <nestorn1515@gmail.com>
2022-06-23 16:48:40 +0200

    Optimizado

--- src/entornos3t2/Entornos3T2.java
+++ src/entornos3t2/Entornos3T2.java
@@ -35,11 +35,13 @@ public class Entornos3T2 {
     }
 
     public static void capturarLista(String[] pakemon, Pakemon juego1) {
-        for (int i = 0; i < pakemon.length; i++) {
+        int longitud = pakemon.length;//he puesto la longitud en una variable porque sino en cada vuelta del bucle tiene que ir a contar la longitud.
+        for (int i = 0; i < longitud; i++) { 
             if (juego1.getPakeballs() > 0) {
                 juego1.capturarPakemon(pakemon[i]);
             } else {
                 System.out.println("No tienes Pakeballs");
+                break;//le he puesto un break para que si no tiene pokeballs el bucle no siga ejecutandose.
             }
         }
     }
