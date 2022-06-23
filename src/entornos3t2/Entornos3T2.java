d8fdc50b705c8f1a8a8eedfcb88d807be4417d22
Javier Merida <javiervldesti@gmail.com>
2022-06-23 16:32:11 +0200

    Optimizada la clase Entornos3T2 y subida captura EJ5

--- src/entornos3t2/Entornos3T2.java
+++ src/entornos3t2/Entornos3T2.java
@@ -30,7 +30,8 @@ public class Entornos3T2 {
     }
 
     public static void capturarLista(String[] pakemon, Pakemon juego1) {
-        for (int i = 0; i < pakemon.length; i++) {
+        int longi = pakemon.length; //Guardo la longitud del array pakemon
+        for (int i = 0; i < longi ; i++) {
             if (juego1.getPakeballs() > 0) {
                 juego1.capturarPakemon(pakemon[i]);
             } else {
